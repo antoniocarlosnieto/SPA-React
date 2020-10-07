acdb0efdbc847923bc928485e2995884caa47c82
Antonio Nieto <antoniocarlosnieto@gmail.com>
2020-10-06 21:56:06 -0300

    Pagina React

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import Parceiros from "./components/parceiros"
 import Servicos from "./components/servicos"
 import Sobre from "./components/sobre"
 
+//o App puxa do .js que fica na pasta Component
+//o .js puxa do .css que fica na pagina Style.
+
+
 class App extends Component {
   render(){
     return(
       <div className= "App">
         <p id= "home">Página de Exercício React</p>
-        <a href= "#home">Home</a> | <a>Contato</a> | <a>Parceiros</a> | <a>Serviços</a> | <a>Sobre</a> | <a>Rodape</a>
+        <a href= "#home">Home</a> | <a href= "">Contato</a> | <a>Parceiros</a> | <a>Serviços</a> | <a>Sobre</a> | <a>Rodape</a>
+
         <Sobre></Sobre>
         <Contato></Contato>
         <Parceiros></Parceiros>
