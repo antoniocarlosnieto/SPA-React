5a4f994bf7369bb2d133f8f689886d3805cda014
Antonio Nieto <antoniocarlosnieto@gmail.com>
2020-10-08 22:05:09 -0300

    atualizacao

--- src/components/topo.js
+++ src/components/topo.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import "../styles/topo.css"
 
+
 class Topo extends Component {
     render(){
         return(
@@ -14,11 +15,16 @@ class Topo extends Component {
                          <a href= "#home">Home</a> | <a href= "#contato">Contato</a> | <a href= "#parceiros">Parceiros</a> | <a href= "#servicos">Serviços</a> | <a href= "#sobre">Sobre</a> | <a href= "#rodape">Rodape</a>
                     </strong>
                 </nav>
-
-               
+          
                 <p className="fonte-da-letra" className="letra-branca" ><h1><h1><strong>Banda Hugs</strong></h1></h1></p>
             
                 <p id= "home"><h3>Fazendo a sua Festa</h3></p>
+
+               
+
+                
+
+              
                
                 
            </div>
